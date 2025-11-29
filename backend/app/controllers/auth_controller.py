2efb3a6908a2ea16e8e545b44c71867258f54d00
Ana Carolina Rocha <ananinhaz@users.noreply.github.com>
2025-11-29 20:00:48 -0300

    ajuste

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify, redirect, current_app
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import check_password_hash
 from app.extensions.db import db
-from .oauth import oauth, safe_authorize_access_token
+from app.oauth import oauth, safe_authorize_access_token
 import os
 
 from app.controllers.usuario_controller import User  # <-- seu User real
